e8f15d442a99e55a999da5137a4631d59230d3b1
Christian Hernandez <chrissh443@gmail.com>
2021-01-12 04:32:31 -0400

    added border radius previewer

---
+++ 1-Beginner/border-rad-preview/app.js
@@ -0,0 +1,15 @@
+
+let morphs = document.querySelectorAll('.square');
+let slider = document.getElementById('slider');
+let output = document.getElementById('slider-value');
+
+slider.addEventListener('change', function() {
+	changeValue();
+});
+
+function changeValue(e) {
+	for(var i=0; i<morphs.length; i++) {
+	  morphs[i].style.borderRadius = slider.value + "%";
+	}
+	output.value = slider.value;
+}
